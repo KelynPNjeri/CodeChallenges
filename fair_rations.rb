f0e0a8522c8eadb9878084faebaf21cd85044083
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-05 19:05:16 +0300

    Start fair rations problem.

---
+++ fair_rations.rb
@@ -0,0 +1,3 @@
+def fairRations(B)
+    
+end
--- manasa_stones.rb
+++ manasa_stones.rb
@@ -1,10 +1,6 @@
 require 'set'
+
 def stones(n, a, b)
-    # a and b are differences
-    # n = number of stones
-    # Permutation 1: [0, a, a+b]
-    # Permutation 2: 
-    # perm = [[a, b], [a, b]]
     perm_set = SortedSet.new([0])
     (n - 1).times do 
         sum_set = SortedSet.new
@@ -13,7 +9,7 @@ def stones(n, a, b)
             sum_set << el + a
             sum_set << el + b
         end
-        perm_set = sum_set  
+        perm_set = sum_set 
     end
     perm_set.to_a
 end
