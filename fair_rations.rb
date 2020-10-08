5546155ba80ad276a881a4c768fb4c6d56df814c
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-08 17:47:07 +0300

    Finish fair rations.

--- fair_rations.rb
+++ fair_rations.rb
@@ -1,3 +1,19 @@
 def fairRations(B)
-    
+    # Step 1: Loop through the arr.
+    # Step 2: Perform before arithmetic.
+    # Step 3: Perform after arithmetic.
+    # Step 4: Have a variable that counts the number of moves made to make loaves even.
+    arr_len = B.length
+    bread_count = 0
+    0.upto(arr_len-1) do |idx|
+      begin
+        if B[idx] % 2 == 1
+          bread_count += 2
+          B[idx+1] += 1
+        end
+      rescue
+        return 'NO'
+      end
+    end
+    return bread_count
 end
